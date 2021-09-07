49305bcee3dea3813dc4102adc3d5e881f241454
Jose Bautista <Cooldj1020@gmail.com>
2021-09-07 17:15:00 -0600

    creating tweets with fake data

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('#tweet-text').on('input', charCounter);
+});
+
+const charCounter = function () {
+  // Finds the length of the input
+  const input = $(this).val().length;
+  const outputLength = 140 - input;
+  $('#chars').text(outputLength);
+  if (outputLength < 0) {
+    // If output is less than 0 the counter and the words will turn red.
+    $('#chars').css({'color': 'red'});
+    $('#tweet-text').css({'color': 'red'});
+  }
+  if (outputLength > 0) {
+    // If output is less than 0 the counter and the words will turn black again.
+    $('#chars').css({'color': 'black'});
+    $('#tweet-text').css({'color': 'black'});
+  }
+};
